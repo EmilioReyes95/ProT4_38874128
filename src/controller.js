200bd1a2f33559b8e7db3b687e5b83eb4c00676b
emilio <emilioreyesfree@hotmail.com>
2024-08-17 20:44:49 -0300

    creacion de base de datos y archivo controller

---
+++ src/controller.js
@@ -0,0 +1,18 @@
+//aca se va a ir poniendo las funcionalidades, consultas en este caso a la base de dato, que va a realizar nuestro programa
+//se trae el pool generado en database: el pool habilita conexiones paralelas o concurrentes
+import {pool} from './database.js';
+
+//se crea clase librocontroller con funcionalidades CRUD para traer lo que tengamos en BD
+
+class LibroController{
+    async getAll(req, res) {
+        const [result] = await pool.query('SELECT * FROM libros');
+        res.json(result);
+    }
+
+}
+//con async (anteriormente al nombre del metodo) y await (en la consulta a la bd que no queremos en stand by) nuestro servidor puede recibir varias consultas 
+
+// se crea una constante para exportar y que sea visible en otros archivos del proyecto
+
+export const libro = new LibroController(); //se crea constante "libro" que va a ser constante en otros archivos del proyecto
